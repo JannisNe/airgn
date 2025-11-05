b207cebfc2d265624cf100b00ad982aa4d879b29
JannisNe <necker@physik.hu-berlin.de>
2025-11-05 14:45:29 +0100

    infrastructiure for legacy survey

---
+++ airgn/legacy_survey/download.py
@@ -0,0 +1,116 @@
+import logging
+import re
+import requests
+from argparse import ArgumentParser
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+from astropy.config import get_cache_dir
+
+
+logger = logging.getLogger(
+    __name__ if __name__ != "__main__" else "airgn.legacy_survey.download"
+)
+BASE_URL = "https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr10/south/sweep/"
+VERSION = "10.1"
+
+
+def get_filenames() -> list[tuple[str, str]]:
+    cache_file = Path(get_cache_dir()) / "legacy_survey_lc_filenames.txt"
+
+    if not cache_file.exists():
+        url = BASE_URL + VERSION + "/"
+        logger.debug(f"fetching lightcurve filenames from {url}")
+        response = requests.get(BASE_URL + VERSION)
+        response.raise_for_status()
+
+        filenames = sorted(
+            list(
+                set(
+                    re.findall(
+                        r"sweep-\d+[mp]\d+-\d+[mp]\d+\.fits",
+                        response.content.decode(),
+                    )
+                )
+            )
+        )
+
+        logger.debug(f"caching lightcurve filenames to {cache_file}")
+        with open(cache_file, "w") as f:
+            for filename in filenames:
+                f.write(f"{filename}\n")
+
+    logger.debug(f"loading lightcurve filenames from {cache_file}")
+    with open(cache_file, "r") as f:
+        filenames = [line.strip() for line in f.readlines()]
+
+    logger.debug(f"found {len(filenames)} lightcurve filenames")
+
+    full_filenames = [
+        (
+            VERSION + "/" + filename,
+            VERSION + "-lightcurves/" + filename.replace(".fits", "-lc.fits"),
+        )
+        for filename in filenames
+    ]
+
+    return full_filenames
+
+
+def get_local_path(filename: str) -> Path:
+    cache_dir = Path(get_cache_dir()) / f"legacy_survey_DR{VERSION}"
+    local_filename = cache_dir / filename
+    local_filename.parent.mkdir(parents=True, exist_ok=True)
+    return local_filename
+
+
+def download_file_by_index(
+    i: int | list[int],
+) -> tuple[Path, Path] | list[tuple[Path, Path]]:
+    filenames = get_filenames()
+    if isinstance(i, int):
+        i = [i]
+    paths: list[tuple[Path, Path]] = []
+    for index in i:
+        if index < 0 or index >= len(filenames):
+            raise IndexError("Index out of range")
+        i_filenames = filenames[index]
+        sub_paths: tuple[Path, Path] = tuple([get_local_path(fn) for fn in i_filenames])  # type: ignore
+        for sub_filename, local_path in zip(i_filenames, sub_paths):
+            if not local_path.exists():
+                url = BASE_URL + sub_filename
+                logger.info(f"Downloading {url} to {local_path}...")
+                response = requests.get(url, stream=True)
+                response.raise_for_status()
+                with open(local_path, "wb") as f:
+                    for chunk in tqdm(
+                        response.iter_content(chunk_size=int(2**20)),
+                        desc=f"Downloading {sub_filename}",
+                        unit="MB",
+                        unit_scale=True,
+                    ):
+                        f.write(chunk)
+
+        paths.append(tuple(sub_paths))
+
+    return paths[0] if len(paths) == 1 else paths
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Download Legacy Survey lightcurve FITS files by index."
+    )
+    parser.add_argument(
+        "indices",
+        metavar="N",
+        type=int,
+        nargs="+",
+        help="Indices of the lightcurve files to download.",
+    )
+    parser.add_argument("--log-level", default="INFO", help="Set the logging level")
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log_level.upper())
+    downloaded_files = download_file_by_index(args.indices)
+    for file in np.atleast_1d(downloaded_files):
+        print(f"Downloaded: {file}")
---
+++ ampel/legacy_survey/alert/load/LegacySurveyBrickLoader.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                airgn/ampel/legacy_survey/alert/load/LegacySurveyBrickLoader.py
+# License:             BSD-3-Clause
+# Author:              Jannis Necker <jannis.necker@gmail.com>
+# Date:                05.11.2025
+# Last Modified Date:  05.11.2025
+# Last Modified By:    Jannis Necker <jannis.necker@gmail.com>
+from typing import Dict, Generator
+from pathlib import Path
+
+from astropy.table import Table
+import pandas as pd
+from ampel.abstract.AbsAlertLoader import AbsAlertLoader
+
+from airgn.legacy_survey.download import (
+    get_filenames,
+    get_local_path,
+    download_file_by_index,
+)
+
+LEGACY_SURVEY_WISE_COLUMNS = [
+    "LC_FLUX_W1",
+    "LC_FLUX_W2",
+    "LC_FLUX_IVAR_W1",
+    "LC_FLUX_IVAR_W2",
+    "LC_NOBS_W1",
+    "LC_NOBS_W2",
+    "LC_MJD_W1",
+    "LC_MJD_W2",
+    "LC_FRACFLUX_W1",
+    "LC_FRACFLUX_W2",
+    "LC_RCHISQ_W1",
+    "LC_RCHISQ_W2",
+    "LC_EPOCH_INDEX_W1",
+    "LC_EPOCH_INDEX_W2",
+]
+
+
+class LegacySurveyBrickLoader(AbsAlertLoader[Dict]):
+    """
+    Load alerts from one of more files.
+    """
+
+    # path to timewise download config file
+    file_indices: list[int] | None = None
+
+    # download files if they do not exist locally
+    download_if_missing: bool = False
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        # get all filenames from legacy survey server
+        filenames = get_filenames()
+        if self.file_indices is not None:
+            filenames = [filenames[i] for i in self.file_indices]
+
+        # download files if missing
+        if self.download_if_missing:
+            indices = (
+                range(len(filenames))
+                if self.file_indices is None
+                else self.file_indices
+            )
+            download_file_by_index(indices)
+
+        # generate local filenames
+        local_filenames = [[get_local_path(fn) for fn in pair] for pair in filenames]
+
+        # set up processing generator
+        self._gen = self.iter_lightcurves(local_filenames)
+
+    def iter_lightcurves(
+        self, filenames: list[list[Path]]
+    ) -> Generator[pd.DataFrame, None, None]:
+        # loop over pairs of summary and lightcurve files
+        for summary_fn, lightcurve_fn in filenames:
+            summary_table = (
+                Table.read(summary_fn, format="fits")
+                .to_pandas()
+                .set_index(["RELEASE", "BRICKID", "OBJID"])
+            )
+
+            for row in Table.read(lightcurve_fn, format="fits"):
+                lc = {
+                    col: row[col]
+                    for col in LEGACY_SURVEY_WISE_COLUMNS
+                    if col in row.colnames
+                }
+                cntr = row["RELEASE"], row["BRICKID"], row["OBJID"]
+                ra = summary_table.loc[cntr, "RA"]
+                dec = summary_table.loc[cntr, "DEC"]
+                yield pd.DataFrame(lc).assign(
+                    release=cntr[0], brickid=cntr[1], objid=cntr[2], ra=ra, dec=dec
+                )
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> pd.DataFrame:  # type: ignore
+        return next(self._gen)
