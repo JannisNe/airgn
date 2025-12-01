b9062916d7f899be9e75088460ef575db5c27600
JannisNe <necker@physik.hu-berlin.de>
2025-12-01 16:41:38 +0100

    fix plotting LS results

--- ampel/legacy_survey/CompareTimewiseToLegacySurvey.py
+++ ampel/legacy_survey/CompareTimewiseToLegacySurvey.py
@@ -26,8 +26,10 @@ class CompareTimewiseToLegacySurvey(AbsPhotoT3Unit):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._base_path = expand(self.base_path)
-        self._base_path.parent.mkdir(exist_ok=True, parents=True)
+        base_path = expand(self.base_path)
+        self._plot_dir = base_path.parent
+        self._plot_dir.mkdir(exist_ok=True, parents=True)
+        self._name = base_path.name
 
     def process(
         self, gen: Generator[TransientView, T3Send, None], t3s: None | T3Store = None
@@ -43,18 +45,27 @@ class CompareTimewiseToLegacySurvey(AbsPhotoT3Unit):
             if t2ls.empty or t2tw.empty:
                 continue
 
-            median_ratios = [
-                t2ls[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].median()
-                / t2tw[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].median()
-                for i in range(1, 3)
-            ]
+            median_ratios = []
+            for i in range(1, 3):
+                lsmed = t2ls[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].median()
+                lsstd = t2ls[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].std()
+                twmed = t2tw[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].median()
+                twstd = t2tw[f"w{i}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"].std()
+                median_ratios.append(
+                    (twmed - lsmed) / np.sqrt(twstd**2 + lsstd**2)
+                    if (twstd > 0 and lsstd > 0)
+                    else np.nan
+                )
 
             if (
                 (self.threshold_to_plot is not None)
                 and (plot_ctr < self.max_plot)
-                and (10 ** max(abs(np.log10(median_ratios))) > self.threshold_to_plot)
+                and (
+                    10 ** max(abs(np.log10(np.abs(median_ratios))))
+                    > self.threshold_to_plot
+                )
             ):
-                self.plot_lightcurves(t2ls, t2tw, view)
+                self.plot_lightcurves(t2ls, t2tw, view, median_ratios)
                 plot_ctr += 1
 
             res.append(median_ratios)
@@ -67,7 +78,9 @@ class CompareTimewiseToLegacySurvey(AbsPhotoT3Unit):
         ax.set_xlabel("Log10(Median Legacy Survey Flux / Median Timewise Flux)")
         ax.set_ylabel("Count")
         ax.legend()
-        fn = self._base_path / "compare_timewise_legacy_survey_median_ratio_hist.pdf"
+        fn = self._plot_dir / (
+            self._name + "compare_timewise_legacy_survey_median_ratio_hist.pdf"
+        )
         fig.suptitle("Median Flux Ratio: Legacy Survey vs Timewise")
         fig.savefig(fn)
         plt.close(fig)
@@ -75,7 +88,11 @@ class CompareTimewiseToLegacySurvey(AbsPhotoT3Unit):
         return None
 
     def plot_lightcurves(
-        self, t2ls: pd.DataFrame, t2tw: pd.DataFrame, view: TransientView
+        self,
+        t2ls: pd.DataFrame,
+        t2tw: pd.DataFrame,
+        view: TransientView,
+        median_ratios: list[float],
     ) -> None:
         fig, ax = plt.subplots()
         plot_lightcurve(
@@ -85,14 +102,35 @@ class CompareTimewiseToLegacySurvey(AbsPhotoT3Unit):
             add_to_label=" Timewise",
             colors={"w1": "lightcoral", "w2": "maroon"},
         )
-        plot_lightcurve(
-            lum_key=keys.FLUX_DENSITY_EXT,
-            stacked_lightcurve=t2ls,
-            ax=ax,
-            add_to_label=" Legacy Survey",
-            colors={"w1": "lightseeblue", "w2": "navy"},
-        )
-        fn = self._base_path / f"{view.id}_compare_timewise_legacy_survey.pdf"
+
+        lsc = {"w1": "lightsteelblue", "w2": "navy"}
+        for b in ["w1", "w2"]:
+            ax.errorbar(
+                t2ls[f"LC_MJD_{b.upper()}"],
+                t2ls[f"{b}{keys.MEAN}{keys.FLUX_DENSITY_EXT}"],
+                yerr=t2ls[f"{b}{keys.FLUX_DENSITY_EXT}{keys.RMS}"],
+                label=f"{b} Legacy Survey",
+                ls="",
+                marker="s",
+                c=lsc[b],
+                markersize=4,
+                markeredgecolor="none",
+                ecolor=lsc[b],
+                capsize=2,
+                zorder=3,
+                barsabove=True,
+                elinewidth=0.5,
+            )
+
+        l = f"W1 median ratio: {median_ratios[0]:.2f}, W2 median ratio: {median_ratios[1]:.2f}"
+        ax.set_title(l)
+
+        ax.set_ylabel("Flux Density (mJy)")
+        ax.set_xlabel("MJD")
+        ax.legend()
+        d = self._plot_dir / self._name
+        d.mkdir(exist_ok=True, parents=True)
+        fn = d / f"{view.id}_compare_timewise_legacy_survey.pdf"
         fig.suptitle(f"Transient {view.id}")
         fig.savefig(fn)
         plt.close(fig)
