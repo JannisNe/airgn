ceb796a0f46f9c068d2e5f225446825a642d7d4f
Jannis Necker <jannis.necker@gmail.com>
2026-01-15 12:47:21 +0100

    make DR and SV an argument

--- airgn/legacy_survey/download.py
+++ airgn/legacy_survey/download.py
@@ -12,18 +12,22 @@ from tqdm import tqdm
 logger = logging.getLogger(
     __name__ if __name__ != "__main__" else "airgn.legacy_survey.download"
 )
-BASE_URL = "https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr10/south/sweep/"
-VERSION = "10.1"
-DATA_DIR = Path(os.environ["AIRGNDATA"]) / "legacy_survey"
+BASE_URL = "https://portal.nersc.gov/cfs/cosmo/data/legacysurvey/dr{DR}/south/sweep/"
+BASE_DATA_DIR = Path(os.environ["AIRGNDATA"]) / "legacy_survey"
 
 
-def get_filenames() -> list[tuple[str, str]]:
-    cache_file = DATA_DIR / "legacy_survey_lc_filenames.txt"
+def get_data_dir(dr: int):
+    return BASE_DATA_DIR / f"legacy_survey_dr{dr}"
 
+
+def get_filenames(dr: int, sv: int) -> list[tuple[str, str]]:
+    data_dir = get_data_dir(dr)
+    cache_file = data_dir / "lc_filenames.txt"
+    version = f"{dr}.{sv}"
     if not cache_file.exists():
-        url = BASE_URL + VERSION + "/"
+        url = BASE_URL.format(DR=dr) + version + "/"
         logger.debug(f"fetching lightcurve filenames from {url}")
-        response = requests.get(BASE_URL + VERSION)
+        response = requests.get(url)
         response.raise_for_status()
 
         filenames = sorted(
@@ -51,8 +55,8 @@ def get_filenames() -> list[tuple[str, str]]:
 
     full_filenames = [
         (
-            VERSION + "/" + filename,
-            VERSION + "-lightcurves/" + filename.replace(".fits", "-lc.fits"),
+            version + "/" + filename,
+            version + "-lightcurves/" + filename.replace(".fits", "-lc.fits"),
         )
         for filename in filenames
     ]
@@ -60,28 +64,29 @@ def get_filenames() -> list[tuple[str, str]]:
     return full_filenames
 
 
-def get_local_path(filename: str) -> Path:
-    cache_dir = DATA_DIR / f"legacy_survey_DR{VERSION}"
-    local_filename = cache_dir / filename
+def get_local_path(filename: str, dr: int) -> Path:
+    local_filename = get_data_dir(dr) / filename
     local_filename.parent.mkdir(parents=True, exist_ok=True)
     return local_filename
 
 
 def download_file_by_index(
-    i: int | list[int],
-) -> tuple[Path, Path] | list[tuple[Path, Path]]:
-    filenames = get_filenames()
+    i: int | list[int], dr: int, sv: int
+) -> tuple[Path, ...] | list[tuple[Path, Path]]:
+    filenames = get_filenames(dr, sv)
     if isinstance(i, int):
         i = [i]
-    paths: list[tuple[Path, Path]] = []
+    paths: list[tuple[Path, ...]] = []
     for index in i:
         if index < 0 or index >= len(filenames):
             raise IndexError("Index out of range")
         i_filenames = filenames[index]
-        sub_paths: tuple[Path, Path] = tuple([get_local_path(fn) for fn in i_filenames])  # type: ignore
+        sub_paths: tuple[Path, ...] = tuple(
+            [get_local_path(fn, dr) for fn in i_filenames]
+        )  # type: ignore
         for sub_filename, local_path in zip(i_filenames, sub_paths):
             if not local_path.exists():
-                url = BASE_URL + sub_filename
+                url = BASE_URL.format(DR=dr) + sub_filename
                 logger.info(f"Downloading {url} to {local_path}...")
                 response = requests.get(url, stream=True)
                 response.raise_for_status()
@@ -110,9 +115,11 @@ if __name__ == "__main__":
         nargs="+",
         help="Indices of the lightcurve files to download.",
     )
+    parser.add_argument("dr", type=int, help="data release number")
+    parser.add_argument("sv", type=int, help="subversion of data release")
     parser.add_argument("--log-level", default="INFO", help="Set the logging level")
     args = parser.parse_args()
     logging.basicConfig(level=args.log_level.upper())
-    downloaded_files = download_file_by_index(args.indices)
+    downloaded_files = download_file_by_index(args.indices, args.dr, args.sv)
     for file in np.atleast_1d(downloaded_files):
         print(f"Downloaded: {file}")
