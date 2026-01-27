b68378c8e5cdcd12bbd9bb17e2704eb89da28029
Jannis Necker <jannis.necker@gmail.com>
2026-01-27 17:24:00 +0100

    add is_in_range()

--- ampel/util/NPointsIterator.py
+++ ampel/util/NPointsIterator.py
@@ -10,12 +10,20 @@ class NPointsIterator(AmpelABC):
     n_points_bins: tuple[int, ...] = (0, 10, 20, 30)
     n_point_cols: list[str]
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._min_edge = min(self.n_points_bins)
+        self._max_edge = max(self.n_points_bins)
+
+    def is_in_range(self, val: float) -> bool:
+        return self._min_edge <= val <= self._max_edge
+
     def iter_npoints_binned(
         self, df: pd.DataFrame
     ) -> Generator[tuple[pd.DataFrame, float, float], None, None]:
-        bins = list(pairwise(self.n_points_bins)) + [
-            (min(self.n_points_bins), max(self.n_points_bins))
-        ]
+        bins = list(pairwise(self.n_points_bins))
+        if all_bin := (min(self.n_points_bins), max(self.n_points_bins)) not in bins:
+            bins.append(all_bin)
         for s, e in bins:
             bin_mask = (df[self.n_point_cols] >= s).all(axis=1) & (
                 df[self.n_point_cols] < e
