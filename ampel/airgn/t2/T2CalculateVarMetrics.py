b673722c3310308aa7d40a619ab71f29e16e40ee
Jannis Necker <jannis.necker@gmail.com>
2026-01-27 19:17:26 +0100

    set excess variance back to linear

--- ampel/airgn/t2/T2CalculateVarMetrics.py
+++ ampel/airgn/t2/T2CalculateVarMetrics.py
@@ -207,7 +207,10 @@ def inverse_von_neumann_ratio(
 
 
 @T2CalculateVarMetrics.register(
-    log=True, range=(-2, 1), pretty_name=r"$\sigma^2_\mathrm{rms}$", multiband=False
+    log=False,
+    range=(-0.1, 0.1),
+    pretty_name=r"$\sigma^2_\mathrm{rms}$",
+    multiband=False,
 )
 def normalized_excess_variance(
     f: float_arr, fe: float_arr, t: float_arr
