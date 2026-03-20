b36f0e9dd52c6df3d6ab6ecf8e5e7580c02e0ed4
Jannis Necker <jannis.necker@gmail.com>
2026-03-20 16:06:29 +0100

    add detection significance, fix min detections

--- ampel/legacy_survey/LegacySurveyWISEFilter.py
+++ ampel/legacy_survey/LegacySurveyWISEFilter.py
@@ -14,29 +14,32 @@ from ampel.protocol.AmpelAlertProtocol import AmpelAlertProtocol
 
 class LegacySurveyWISEFilter(AbsAlertFilter):
     min_exp_per_visit: int = 8
+    detection_significance: float = 5
     min_detections: int = 10
 
     def process(self, alert: AmpelAlertProtocol) -> None | bool | int:
         # enough single exposures per visit
         min_exp_per_visit = np.min(
-            [[dp[f"LC_NOBS_W{i}"] for dp in alert.datapoints for i in range(1, 3)]]
+            [dp[f"LC_NOBS_W{i}"] for dp in alert.datapoints for i in range(1, 3)]
         )
         if not min_exp_per_visit >= self.min_exp_per_visit:
             self.logger.debug(None, extra={"min_exp_per_visit": min_exp_per_visit})
             return None
 
         # enough detections
-        min_detections = np.min(
+        min_detections = np.sum(
             [
                 [
-                    dp[f"LC_FLUX_W{i}"] * np.sqrt(dp[f"LC_FLUX_IVAR_W{i}"])
+                    (dp[f"LC_FLUX_W{i}"] * np.sqrt(dp[f"LC_FLUX_IVAR_W{i}"]))
+                    > self.detection_significance
                     for dp in alert.datapoints
                 ]
                 for i in range(1, 3)
-            ]
-        )
+            ],
+            axis=1,
+        ).min()
         if not min_detections >= self.min_detections:
-            self.logger.debug(None, extra={"n_detections": len(min_detections)})
+            self.logger.debug(None, extra={"n_detections": min_detections})
             return None
 
         return True
