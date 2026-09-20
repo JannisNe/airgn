a63c665dd3f8e1092c9dce566967197d7fb21b26
Jannis Necker <jannis.necker@gmail.com>
2026-09-20 17:32:13 +0200

    add hist

--- ampel/airgn/t3/CompareAGNXGBResult.py
+++ ampel/airgn/t3/CompareAGNXGBResult.py
@@ -14,7 +14,7 @@ from timewise.util.path import expand
 class CompareAGNXGBResult(AbsPhotoT3Unit):
     prepended_keys: tuple[str, str]
     labels: tuple[str, str]
-    filename: str
+    filename_base: str
     mplstyle: str | None = None
 
     def process(
@@ -29,7 +29,7 @@ class CompareAGNXGBResult(AbsPhotoT3Unit):
         for k in ["agn", "wise_agn"]:
             assert all(data[f"{ppk[0]}_{k}"] == data[f"{ppk[0]}_{k}"])
 
-        non_agn_mask = ~data[f"{ppk[0]}_agn"].astype(bool)
+        non_agn_mask = ~(data[f"{ppk[0]}_agn"].astype(bool))
         wise_agn_mask = data[f"{ppk[0]}_wise_agn"].astype(bool)
         non_wise_agn_mask = ~non_agn_mask & ~wise_agn_mask
         masks = [non_agn_mask, wise_agn_mask, non_wise_agn_mask]
@@ -48,15 +48,35 @@ class CompareAGNXGBResult(AbsPhotoT3Unit):
                 data.loc[m, f"{ppk[0]}_probability"],
                 data.loc[m, f"{ppk[1]}_probability"],
                 zorder=10,
+                s=1,
+                alpha=0.1,
             )
             ax.plot(xx, xx, ls=":", zorder=1, alpha=0.3)
             ax.set_title(title)
 
-        axs[0].set_ylabel(labels[0])
-        axs[1].set_xlabel(labels[1])
+        fig.supxlabel(self.labels[0])
+        fig.supylabel(self.labels[1])
 
-        fn = expand(self.filename)
-        fn.parent.mkdir(parents=True, exist_ok=True)
+        fn_base = expand(self.filename_base)
+        fn_base.parent.mkdir(parents=True, exist_ok=True)
+        fn = str(fn_base) + "_scatter.png"
+        fig.tight_layout()
+        fig.savefig(fn)
+        plt.close()
+
+        fig, axs = plt.subplots(
+            ncols=3, figsize=(fs[0] * 2, fs[1]), sharex=True, sharey=True
+        )
+        for m, ax, title in zip(masks, axs, labels):
+            ratio = (
+                data.loc[m, f"{ppk[0]}_probability"]
+                / data.loc[m, f"{ppk[1]}_probability"]
+            )
+            ax.hist(np.log10(ratio), ec="white", alpha=0.8, bins=20, density=True)
+            ax.set_title(title)
+        fig.supylabel("density")
+        fig.supxlabel(f"{self.labels[0]} / {self.labels[1]}")
+        fn = str(fn_base) + "_hist.pdf"
         fig.tight_layout()
         fig.savefig(fn)
         plt.close()
